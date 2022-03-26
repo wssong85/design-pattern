d39e1ab2ccb5517c70af80a43cd20ffbabf803cd
songwonseock <tablier@gmail.com>
2022-03-27 00:38:05 +0900

    builder add

---
+++ design_parttern/src/builder/Main.java
@@ -0,0 +1,9 @@
+package builder;
+
+public class Main {
+
+  public static void main(String[] args) {
+    String toString = PersonalInfo.builder().name("이름").age(1).phoneNUmber(023213333).build().toString();
+    System.out.println("toString = " + toString);
+  }
+}
---
+++ design_parttern/src/builder/PersonalInfo.java
@@ -0,0 +1,53 @@
+package builder;
+
+public class PersonalInfo {
+  private String name;
+  private int age;
+  private int phoneNumber;
+
+  public PersonalInfo() {
+
+  }
+
+  @Override
+  public String toString() {
+    return "PersonalInfo{" +
+        "name='" + name + '\'' +
+        ", age=" + age +
+        ", phoneNumber=" + phoneNumber +
+        '}';
+  }
+
+  public static Builder builder() {
+    return new Builder();
+  }
+
+  public static class Builder{
+    private String name;
+    private int age;
+    private int phoneNumber;
+
+    public Builder name(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Builder age(int age) {
+      this.age = age;
+      return this;
+    }
+
+    public Builder phoneNUmber(int phoneNumber) {
+      this.phoneNumber = phoneNumber;
+      return this;
+    }
+
+    public PersonalInfo build() {
+      PersonalInfo personalInfo = new PersonalInfo();
+      personalInfo.name = name;
+      personalInfo.age = age;
+      personalInfo.phoneNumber = phoneNumber;
+      return personalInfo;
+    }
+  }
+}
