28f2cba5c41b3489864fa6b5d69c226e9cd61f7d
songwonseock <tablier@gmail.com>
2022-03-26 21:14:30 +0900

    iterator add

--- design_parttern/src/observer/Main.java
+++ design_parttern/src/observer/Main.java
@@ -7,13 +7,16 @@ public class Main {
     User1 user1 = new User1("유저1");
     User2 user2 = new User2("유저2");
 
+    // attach
     notice.attach(user1);
     notice.attach(user2);
 
     String msg = "공지사항입니다~!";
     notice.notifyObservers(msg);
 
+    // detach
     notice.detach(user1);
+
     msg = "안녕하세요~";
     notice.notifyObservers(msg);
   }
